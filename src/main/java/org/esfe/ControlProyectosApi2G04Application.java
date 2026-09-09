73591ea4fde63ecc51e60dea95a0dd210ed491e1
victor1206 <victor_rivas89@hotmail.com>
2026-09-08 20:34:52 -0600

    primer commit

---
+++ src/main/java/org/esfe/ControlProyectosApi2G04Application.java
@@ -0,0 +1,13 @@
+package org.esfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlProyectosApi2G04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlProyectosApi2G04Application.class, args);
+	}
+
+}
